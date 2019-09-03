0afd8d2e1e6cae561b0d586195573dfb7f2f9a61
Jacob Hardman <42230736+thejacobhardman@users.noreply.github.com>
2019-09-03 14:20:29 -0600

    Add project files.

---
+++ Basic-Console-Interaction/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Console_Interaction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boolean isRunning = true;
+            Boolean userConfirm = false;
+            string input = "";
+
+            while(isRunning)
+            {
+                Console.WriteLine("Enter first integer: ");
+                int num1 = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Enter second integer: ");
+                int num2 = Int32.Parse(Console.ReadLine());
+
+                int sum = num1 + num2;
+                int product = num1 * num2;
+                int difference = num1 - num2;
+                int quotient = num1 / num2;
+
+                Console.WriteLine("The sum of the two numbers is: " + sum);
+                Console.WriteLine("The product of the two numbers is: " + product);
+                Console.WriteLine("The difference of the two numbers is: " + difference);
+                Console.WriteLine("The quotient of the two numbers is: " + quotient);
+
+                while (userConfirm == false)
+                {
+                    Console.WriteLine("Would you like to run the program again? (Y/N)");
+                    input = Console.ReadLine().ToUpper();
+
+                    if (input == "Y")
+                    {
+                        userConfirm = true;
+                        continue;
+                    }
+                    else if (input == "N")
+                    {
+                        userConfirm = true;
+                        isRunning = false;
+                        break;
+                    } else
+                    {
+                        Console.WriteLine("I'm sorry, I don't understand.");
+                    }
+                }
+            }
+        }
+    }
+}
